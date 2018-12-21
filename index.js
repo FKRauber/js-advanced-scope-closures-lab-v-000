df518e4b697d9f0b1965bcc8e1f417822f5c1d9d
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-21 04:43:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-function produceDrivingRange(blockRange){
-  return function(start, end){
-
+function produceDrivingRange(blockRange) {
+  return function(start, end) {
     let s = parseInt(start);
     let e = parseInt(end);
-    let distanceToTravel = Math.abs(e - s);
-    let difference = blockRange - distanceToTravel;
+    let distance = Math.abs(e - s);
+    let difference = blockRange - distance;
 
-    if( difference > 0){
-      return `within range by ${difference}`
+    if (difference > 0) {
+      return 'within range by $(difference)'
     } else {
-      return `${Math.abs(difference)} blocks out of range`
+      return '${Math.abs(difference)} blocks out of range'
     }
   }
-}e
+}
