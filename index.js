1abf6ec66f85d2f64e24964e926dac8c6f26c337
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-21 04:47:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function produceDrivingRange(blockRange){
+  return function(start, end){
+
+    let s = parseInt(start);
+    let e = parseInt(end);
+    let distanceToTravel = Math.abs(e - s);
+    let difference = blockRange - distanceToTravel;
+
+    if( difference > 0){
+      return `within range by ${difference}`
+    } else {
+      return `${Math.abs(difference)} blocks out of range`
+    }
+  }
+}
+
+
+function produceTipCalculator(billPrice){
+  return function(tip){
+    return billPrice * tip;
+  };
+}
+
+function createDriver(){
+  let driverId = 0;
+
+  return class {
+    constructor(name){
+        this.name = name;
+        this.id = ++driverId;
+    }
+  };
+}
