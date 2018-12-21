e4564fe0b901793b33e713e85088289326f08fde
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-12-21 04:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 function produceDrivingRange(blockRange) {
   return function(start, end) {
-    if ((blockRange - Math.abs(parseInt(end) - parseInt(start))) >0 ) {
-      return 'within range by' + (blockRange -  (parseInt(end) - parseInt(start)));
+    let s = parseInt(start);
+    let e = parseInt(end);
+    let distance = Math.abs(end - start);
+    let difference = blockRange - distance;
+
+    if (difference > 0) {
+      return 'within range by $(difference)'
     } else {
-      return Math.abs(blockRange - (parseInt(end) - parseInt(start))) + 'blocks out of range';
-    };
+      return '${Math.abs(difference)} blocks out of range'
+    }
+  }
+}
+
+function produceTipCalculator(){
+  return function(){
+
+  };
+}
+
+function createDriver(){
+  return class {
+    let driverId = 0;
+    constructor(name){
+        this.name = name;
+        this.id = ++driverId;
+    }
   };
 }
